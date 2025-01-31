02d5751fc8e1a3fd9682d7aa007e0e41853acc75
rudhaesmeraldo <rudhaesmeraldo@gmail.com>
2025-01-31 15:11:30 -0300

    criando sistema de cadastro

--- loja/perfil_usuario/urls.py
+++ loja/perfil_usuario/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from perfil_usuario import views
 
 urlpatterns = [
+    path('cadastrar/', views.cadastrar, name='cadastrar'),
     path('fornecedores/<int:pk>/', views.detalhes_fornecedor, name='detalhes_fornecedor'),
-
 
 ]
--- loja/perfil_usuario/views.py
+++ loja/perfil_usuario/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from perfil_usuario.models import Perfil_usuario
 
 def detalhes_fornecedor(request, pk):
     usuario = User.objects.get(pk=pk)
 
     return render(request,'perfil_usuario/detalhes_fornecedor.html',{'usuario':usuario})
+
+def cadastrar(request):
+    if request.method=='POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            usuario = form.save()
+
+            login(request, usuario)
+            perfil_usuario = Perfil_usuario.objects.create(usuario=usuario)
+
+            return redirect('frontpage')
+    
+    else:
+        formulario = UserCreationForm()
+
+    return render(request,'perfil_usuario/cadastrar.html',{'formulario':formulario})
